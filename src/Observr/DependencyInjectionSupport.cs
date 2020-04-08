62a671072410250b3764af2edc8771d6f6bb9340
Sebastian Schoof <sebastian.schoof@jandenul.com>
2020-04-08 10:18:50 +0200

    Add option to pass ServiceLifetime

--- src/Observr/DependencyInjectionSupport.cs
+++ src/Observr/DependencyInjectionSupport.cs
@@ -9,9 +9,9 @@ namespace Observr
 {
     public static class DependencyInjectionSupport
     {
-        public static IServiceCollection AddObservr(this IServiceCollection services)
+        public static IServiceCollection AddObservr(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
         {
-            services.AddSingleton<IBroker, Broker>();
+            services.Add(new ServiceDescriptor(typeof(IBroker), typeof(Broker), serviceLifetime));
 
             return services;
         }
