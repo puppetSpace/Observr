eabf46d0e95ecd4f65ebaf4558231ba6d6d7c734
puppetSpace <mobscene_dude@hotmail.com>
2021-12-09 09:35:30 +0100

    Update UnSubscriber.cs

--- src/Observr/UnSubscriber.cs
+++ src/Observr/UnSubscriber.cs
@@ -1,19 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Observr
 {
     internal class UnSubscriber<TE> : IDisposable
     {
-        private readonly Dictionary<Type, List<IObserver>> _observers;
-        private readonly IObserver<TE> _observer;
+        private readonly Dictionary<Type, List<ObserverPair>> _observers;
+        private readonly ObserverPair _observer;
 
-        public UnSubscriber(Dictionary<Type, List<IObserver>> observers, IObserver<TE> observer)
+        public UnSubscriber(Dictionary<Type, List<ObserverPair>> observers, ObserverPair observer)
         {
             _observers = observers;
             _observer = observer;
@@ -21,13 +22,11 @@ namespace Observr
 
         public void Dispose()
         {
-            lock (_observers) {
-                if (_observers.ContainsKey(typeof(TE)) && _observers[typeof(TE)].FirstOrDefault(x => x.GetType() == _observer.GetType()) is var observer)
-                {
-                    _observers[typeof(TE)].Remove(observer);
-                    if (!_observers[typeof(TE)].Any())
-                        _observers.Remove(typeof(TE));
-                }
+            if (_observers.ContainsKey(typeof(TE)) && _observers[typeof(TE)].FirstOrDefault(x => x.Guid == _observer.Guid) is var observer)
+            {
+                _observers[typeof(TE)].Remove(observer);
+                if (!_observers[typeof(TE)].Any())
+                    _observers.Remove(typeof(TE));
             }
         }
     }
