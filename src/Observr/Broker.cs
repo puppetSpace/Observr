a8a84b71d6397955ff79b02b5b4017db6ea52247
puppetSpace <mobscene_dude@hotmail.com>
2021-12-09 09:34:00 +0100

    Update Broker.cs

--- src/Observr/Broker.cs
+++ src/Observr/Broker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,48 +8,63 @@ using System.Threading.Tasks;
 
 namespace Observr
 {
-	/// <summary>
-	/// Is a mediator between the objects doing the notifying and objects receiving the notifications
-	/// </summary>
-	public class Broker : IBroker
-	{
-		private readonly Dictionary<Type, List<IObserver>> _observers = new Dictionary<Type, List<IObserver>>();
-		private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+    /// <summary>
+    /// Is a mediator between the objects doing the notifying and objects receiving the notifications
+    /// </summary>
+    public class Broker : IBroker
+    {
+        private readonly Dictionary<Type, List<ObserverPair>> _observers = new Dictionary<Type, List<ObserverPair>>();
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
 
-		public async Task Publish<TE>(TE value, CancellationToken cancellationToken = default)
-		{
-			await _semaphore.WaitAsync();
-			try
-			{
-				if (_observers.ContainsKey(typeof(TE)))
-				{
-				
-					foreach (IObserver<TE> observer in _observers[typeof(TE)])
-						await observer.Handle(value, cancellationToken).ConfigureAwait(false);
+        public async Task Publish<TE>(TE value, CancellationToken cancellationToken = default)
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                if (_observers.ContainsKey(typeof(TE)))
+                {
+                    foreach (var pair in _observers[typeof(TE)].ToList()) // run over list of list doesn't cause concurrency issues
+                    {
+                        var observer = pair.Observer as IObserver<TE>;
+                        await observer.Handle(value, cancellationToken).ConfigureAwait(false);
+                    }
+                }
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
 
-				}
-			} finally{
-				_semaphore.Release();
-			}
-		}
+        public IDisposable Subscribe<TE>(IObserver<TE> observer)
+        {
+            _semaphore.Wait();
+            var pair = new ObserverPair(observer);
+            try
+            {
+                if (_observers.ContainsKey(typeof(TE)) && _observers[typeof(TE)] is object)
+                    _observers[typeof(TE)].Add(pair);
+                else
+                    _observers[typeof(TE)] = new List<ObserverPair> { pair };
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
 
-		public IDisposable Subscribe<TE>(IObserver<TE> observer)
-		{
-			_semaphore.Wait();
-			try
-			{
-				if (_observers.ContainsKey(typeof(TE)) && _observers[typeof(TE)] is object)
-				{
-					_observers[typeof(TE)].Add(observer);
-				}
-				else
-					_observers[typeof(TE)] = new List<IObserver> { observer };
+            return new UnSubscriber<TE>(_observers, pair);
+        }
+    }
 
-			} finally{
-				_semaphore.Release();
-			}
-			
-			return new UnSubscriber<TE>(_observers, observer);
-		}
-	}
+    public class ObserverPair
+    {
+        public IObserver Observer { get; }
+        public Guid Guid { get; }
+
+        public ObserverPair(IObserver observer)
+        {
+            Observer = observer;
+            Guid = Guid.NewGuid();
+        }
+    }
 }
